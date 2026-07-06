5e31393be391001e3a196d4f4eba6a5a9a0fe581
sigepiyo338-droid <sigepiyo338@gmail.com>
2026-07-07 02:28:01 +0900

    False

--- siritori_project/settings.py
+++ siritori_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-x9*e_pvekvx^y#=(9g=c9un4a#6_(ydo7-*c+tmtdp)yi-h*(o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['sigepiyo338.pythonanywhere.com', 'localhost', '127.0.0.1']
 
